d4a7b7b1423f52307b7be04b29bb8e6a3416681f
svizcarragradilla@gmail.com <svizcarragradilla@gmail.com>
2022-07-11 21:36:13 -0500

    Home page and header created

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,27 @@
+import { Movie } from "../typing"
+import Image from 'next/image';
+
+interface Props {
+    movie: Movie 
+    // When using firebase
+    //movie: Movie | DocumentData
+}
+
+const Thumbnail = ({movie}: Props) => {
+  return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition 
+                    duration-200 ease-out md:h-36 md:min-w-[216px] 
+                    md:hover:scale-150"
+    >
+        <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+      />
+    </div>
+  )
+}
+
+export default Thumbnail
